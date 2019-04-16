75bb20a82cd1d90e68ba3de6bda667b20260613c
JKGravelle <gravellejk@g.cofc.edu>
2019-04-15 21:01:20 -0400

    First commit

---
+++ app/src/main/java/com/example/button_counter_gravelle/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.button_counter_gravelle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.button_counter_gravelle.R;
+
+import org.w3c.dom.Text;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout myLayout;
+    private int count;
+    private TextView counterDisplay;
+    private Button addOne;
+    private Button subOne;
+    private Button resetAll;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        myLayout = (LinearLayout) findViewById (R.id.mainLayout);
+        counterDisplay = (TextView) findViewById (R.id.Count);
+        addOne = (Button) findViewById (R.id.plus);
+        subOne = (Button) findViewById (R.id.minus);
+        resetAll = (Button) findViewById (R.id.reset);
+
+        addOne.setOnClickListener (new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View v) {
+                count++;
+                counterDisplay.setText (Integer.toString (count));
+            }
+        });
+
+        subOne.setOnClickListener (new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View v) {
+                count--;
+                counterDisplay.setText (Integer.toString (count));
+            }
+        });
+
+        resetAll.setOnClickListener (new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View v) {
+                count = 0;
+                counterDisplay.setText (Integer.toString (count));
+            }
+        });
+
+
+    }
+}
